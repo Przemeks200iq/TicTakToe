6096c935ce10f3e5c1c0a40dfab3c00cd96c7e58
Przemeks200iq <82707103+Przemeks200iq@users.noreply.github.com>
2022-04-30 19:31:20 +0200

    Add clicking ability

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 'use strict';
+const round = document.getElementById("round");
+let useX = true;
 
 for (let i=1; i<=9; i++)
 {
-    i = i.toString();
 
-    document.getElementById(i).addEventListener('click', () => {
-        alert(i);
+
+    document.getElementById(i.toString()).addEventListener('click', () => {
+        click(i)
     })
 }
 
+const click = (numberOfDiv) => {
+    document.getElementById(numberOfDiv.toString()).innerHTML = useX ? "X" : "0";
+    round.innerHTML = `Tura:${useX ? "0" : "X"}`;
+
+    useX = useX ? false : true;
+}
