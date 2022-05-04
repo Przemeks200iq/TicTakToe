98d02112295a0257fa09e2504bee48d8cc6cf9a3
Przemeks200iq <vertezelopl@gmail.com>
2022-05-04 18:55:46 +0200

    Add restart game and separate index.js to new files

---
+++ src/win.js
@@ -0,0 +1,16 @@
+const giveClassList = () => {
+    for (let i = 1; i <= 9; i++) {
+        document.getElementById(i.toString()).classList.add("off");
+    }
+    playAgain.classList.remove("hidden");
+};
+
+export const winX = () => {
+    round.innerHTML = `Wygrywa:X`;
+    giveClassList();
+};
+
+export const win0 = () => {
+    round.innerHTML = `Wygrywa:0`;
+    giveClassList();
+};
